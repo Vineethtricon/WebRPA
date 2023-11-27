47c8fd0bfa612773ffc6a70bfff17e949690170f
Vineeth <vineethvasudevan@triconinfotech.com>
2023-11-27 13:18:51 +0530

    Updating Test flow Details

--- spec/uiSpec/ui.spec.js
+++ spec/uiSpec/ui.spec.js
@@ -9,6 +9,8 @@ const apiMethods = new uiCommonUtils.apiBaseClass.APIBaseClass()
 1. Lounch Triconinfotech URL 
 2. Navigate to Menu "Industries", "Solution", "Difference"
 3. Validate the Page title 
+4. Grab the page details and stored in to Json 
+5. Hit the POST API call with the same JSON Body and validating 200 respose code
 */
 test('Demo Method for Tricon Module', async ({ request, page }) => {
     const URL = "https://9gc3i32w1h.execute-api.ap-south-1.amazonaws.com/Prod/hello/"
