bbd0fb4f4fe131013d9210e9a8d34e3c3a1e0208
Vineeth <vineethvasudevan@triconinfotech.com>
2023-11-28 21:12:46 +0530

    Updated the code content

--- web-scrapping/scrape.js
+++ web-scrapping/scrape.js
@@ -3,6 +3,22 @@ const fs = require('fs');
 let postURL = 'https://9gc3i32w1h.execute-api.ap-south-1.amazonaws.com/Prod/hello/';
 let uiURL = 'http://www.triconinfotech.com/';
 
+/**
+ * This script uses Playwright to automate browser interactions for web scraping and API communication.
+ * It performs the following actions:
+ * 
+ * 1. Opens a new page in the browser.
+ * 2. Navigates to a specified UI URL (uiURL) to scrape data from it.
+ * 3. Extracts the HTML content of the webpage.
+ * 4. Converts the extracted HTML content to JSON format.
+ * 5. Saves the JSON data to a file named "jsonContent.json".
+ * 6. Makes a POST request to a specified API URL (postURL) sending the JSON data as the payload.
+ * 7. Retrieves the response status from the API call and logs it to the console.
+ * 8. Closes the browser after completing these actions.
+ * 
+ * Note: The URLs (uiURL and postURL) and the specific data extraction logic must be defined according to the user's requirements.
+ */
+
 (async () => {
     const browser = await playwright.chromium.launch();
     const context = await browser.newContext();
