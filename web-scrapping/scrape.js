34ff37b56dfad4da9154006c0d7becf94ab65c88
Vineeth <vineethvasudevan@triconinfotech.com>
2023-11-29 21:57:10 +0530

    Added feedback modification

--- web-scrapping/scrape.js
+++ web-scrapping/scrape.js
@@ -1,14 +1,16 @@
+const http = require('http');
+const https = require('https');
 const playwright = require('playwright');
-const fs = require('fs');
+
+let getURL = 'http://www.triconinfotech.com/';
 let postURL = 'https://9gc3i32w1h.execute-api.ap-south-1.amazonaws.com/Prod/hello/';
-let uiURL = 'http://www.triconinfotech.com/';
 
 /**
  * This script uses Playwright to automate browser interactions for web scraping and API communication.
  * It performs the following actions:
  * 
  * 1. Opens a new page in the browser.
- * 2. Navigates to a specified UI URL (uiURL) to scrape data from it.
+ * 2. Navigates to a specified UI URL (getURL) to scrape data from it.
  * 3. Extracts the HTML content of the webpage.
  * 4. Converts the extracted HTML content to JSON format.
  * 5. Saves the JSON data to a file named "jsonContent.json".
@@ -16,34 +18,104 @@ let uiURL = 'http://www.triconinfotech.com/';
  * 7. Retrieves the response status from the API call and logs it to the console.
  * 8. Closes the browser after completing these actions.
  * 
- * Note: The URLs (uiURL and postURL) and the specific data extraction logic must be defined according to the user's requirements.
+ * Note: The URLs (getURL and postURL) and the specific data extraction logic must be defined according to the user's requirements.
  */
 
-(async () => {
-    const browser = await playwright.chromium.launch();
-    const context = await browser.newContext();
-    const page = await context.newPage();
-    
+/**
+ * Function to make a GET request to a specified URL and return the response body.
+ * @param {string} url - The URL to make the GET request to.
+ */
 
-  await page.goto(uiURL); // Replace with your target URL
+// GET request 
+async function httpGet(url) {
+  return new Promise((resolve, reject) => {
+    const protocol = new URL(url).protocol;
+    const lib = protocol === 'https:' ? https : http;
 
-  // Extract data using selectors
-  const data = await page.evaluateHandle(() => document.body);
-    const dataEvalute = await page.evaluateHandle(body => body.innerHTML, data);
-    const pageJSON = await dataEvalute.jsonValue();
-    await dataEvalute.dispose();
+    const handleResponse = (res) => {
+      if (res.statusCode >= 300 && res.statusCode < 400 && res.headers.location) {
+        // Handle redirect
+        httpGet(res.headers.location).then(resolve).catch(reject);
+      } else if (res.statusCode < 200 || res.statusCode >= 300) {
+        // Handle other HTTP errors
+        reject(new Error('statusCode=' + res.statusCode));
+      } else {
+        let data = '';
+        res.on('data', (chunk) => { data += chunk; });
+        res.on('end', () => { resolve(data); });
+      }
+    };
 
-  // Convert the data to JSON
-  const json = JSON.stringify(pageJSON, null, 2);
-  fs.promises.writeFile("jsonContent.json", json, 'utf-8');
-  // POST API call
-  const apiResponse = await context.request.post(postURL, {
-    data: json,
-    headers: {
-      'Content-Type': 'application/json'
-    }
+    lib.get(url, handleResponse).on('error', reject);
   });
+}
 
-  const responseBody = await apiResponse.status();
-  console.log('API Response:', responseBody);  await browser.close();
+
+/**
+ * Function to make a POST request to a specified URL with JSON data.
+ * @param {string} url - The URL to make the POST request to.
+ * @param {object} jsonData - The JSON data to send in the POST request.
+ */
+
+async function httpPost(url, jsonData) {
+  return new Promise((resolve, reject) => {
+    const data = jsonData;
+    const options = {
+      hostname: new URL(url).hostname,
+      // port: new URL(url).port, //if url has any port numbr use this
+      path: new URL(url).pathname,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': Buffer.byteLength(data)
+      }
+    };
+
+    const protocol = new URL(url).protocol;
+    const lib = protocol === 'https:' ? https : http;
+
+    const req = lib.request(options, (res) => {
+      let responseBody = '';
+      res.setEncoding('utf8');
+
+      res.on('data', (chunk) => { responseBody += chunk; });
+
+      res.on('end', () => {
+        const response = {
+          statusCode: res.statusCode,
+          body: responseBody
+        };
+        resolve(response);
+      });
+    });
+
+    req.on('error', (e) => { reject(e); });
+    req.write(data);
+    req.end();
+  });
+}
+
+
+(async () => {
+  const browser = await playwright.chromium.launch();
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  
+
+await page.goto(getURL); // Replace with your target URL
+
+// Extract data using selectors
+const data = await page.evaluateHandle(() => document.body);
+  const dataEvalute = await page.evaluateHandle(body => body.innerHTML, data);
+  const pageJSON = await dataEvalute.jsonValue();
+  await dataEvalute.dispose();
+
+// Convert the data to JSON
+const json = JSON.stringify(pageJSON, null, 2);
+// POST API call
+const response = await httpPost(postURL, json);
+//console.log('API Response Body:', response.body);
+console.log('API Response Status Code:', response.statusCode);
+ await browser.close();
 })();
+
